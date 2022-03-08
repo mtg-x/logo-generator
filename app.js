25c78bd0f8175480029bc0bc4c7ae018ee50676d
tagazok <leplus.olivier@gmail.com>
2022-03-08 22:32:34 +0100

    feat(placeholders): Add random placeholders

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const downloadLogoDOM = document.querySelector("#downloadLogo");
 communityNameInputDOM.addEventListener('keyup', getCommunityName);
 communityTagsInputDOM.addEventListener('keyup', getCommunityTags);
 
+const citiesList = ['Paris', 'Aix', 'Toulouse', 'Clermont', 'Lille', 'Strasbourg', 'Luxembourg', 'Lyon', 'Grenoble', 'Bordeaux', 'Nantes', 'Rennes', 'Tours'];
+const tagsList = ['Azure', '.NET', 'DevOps', 'TypeScript']
+
+communityNameInputDOM.placeholder = `ex: ${getRandomItem(citiesList)}`;
+communityTagsInputDOM.placeholder = `ex: ${getRandomItem(tagsList)}`;
+
 function getCommunityName() {
     const communityName = communityNameInputDOM.value;
     communityNameDOM.innerHTML = communityName;
@@ -17,4 +23,9 @@ function getCommunityName() {
 function getCommunityTags() {
     const tags = communityTagsInputDOM.value;
     communityTagsDOM.innerHTML = tags;
+}
+
+
+function getRandomItem(cities) {
+    return cities[Math.floor(Math.random() * cities.length)];
 }
